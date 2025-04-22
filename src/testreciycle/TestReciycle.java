9b3d71001b39dcad6ee950537324d6b00885029d
Nestor Bolivar <nbolivar@unal.edu.co>
2025-04-22 15:17:41 -0500

    Se ralizaron los archivos de lla primera tarea

---
+++ src/testreciycle/TestReciycle.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testreciycle;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Nestor Bolivar
+ */
+public class TestReciycle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        System.out.println("Hola mundo, ingrese su nombre ");
+        
+        Integer variableEntero = 21;
+        Float var1;
+        Double var2;
+        String var3;
+        Boolean
+        // realizar una encenta ;
+                
+                //Nombre
+                // carrera
+                //edad
+                //telefono
+                
+                
+        System.out.println(variableEntero.toString());
+        
+        System.out.println("este es un num " + variableEntero);
+        
+    }
+    
+}
